c0d8a4deca0e473b55689d9034de1ecc23e3db22
Scott Lowe <scott.lowe@scottlowe.org>
2017-04-11 12:55:03 -0600

    Update Ansible configuration and security group definition
    Update ansible configuration and security group definition
    
    Signed-off-by: Scott Lowe <scott.lowe@scottlowe.org>

--- ec2-swarm/security.tf
+++ ec2-swarm/security.tf
@@ -4,33 +4,9 @@ resource "aws_security_group" "swarm_sg" {
     name                    = "swarm-sg"
     description             = "Security group for Swarm traffic"
     ingress {
-        from_port           = "2375"
-        to_port             = "2377"
-        protocol            = "tcp"
-        cidr_blocks         = ["${aws_vpc.ec2_swarm_vpc.cidr_block}"]
-    }
-    ingress {
-        from_port           = "7946"
-        to_port             = "7946"
-        protocol            = "tcp"
-        cidr_blocks         = ["${aws_vpc.ec2_swarm_vpc.cidr_block}"]
-    }
-    ingress {
-        from_port           = "7946"
-        to_port             = "7946"
-        protocol            = "udp"
-        cidr_blocks         = ["${aws_vpc.ec2_swarm_vpc.cidr_block}"]
-    }
-    ingress {
-        from_port           = "4789"
-        to_port             = "4789"
-        protocol            = "tcp"
-        cidr_blocks         = ["${aws_vpc.ec2_swarm_vpc.cidr_block}"]
-    }
-    ingress {
-        from_port           = "4789"
-        to_port             = "4789"
-        protocol            = "udp"
+        from_port           = "0"
+        to_port             = "0"
+        protocol            = "-1"
         cidr_blocks         = ["${aws_vpc.ec2_swarm_vpc.cidr_block}"]
     }
     ingress {
